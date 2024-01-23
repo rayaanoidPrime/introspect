59c4b1b65209f62fd6c38f66ee6aff54c6eb9af7
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-01-24 06:21:26 +0800

    Add MS SSO validation endpoint and user creation

--- agents-backend/agents/auth_routes.py
+++ agents-backend/agents/auth_routes.py
@@ -1,8 +1,50 @@
 from fastapi import APIRouter, Request
 from auth_utils import login_user, reset_password, validate_user
+import jwt
+import asyncio
+import hashlib
+from db_utils import get_db_conn
+
+SALT = "TOMMARVOLORIDDLE"
+DEFOG_API_KEY = "genmab-survival-test"
 
 router = APIRouter()
 
+async def async_decode_jwt(token):
+    return jwt.decode(token, options={"verify_signature": False})
+
+@router.post("/validate_ms_sso")
+async def validate_ms_sso(request: Request):
+    params = await request.json()
+    token = params.get("token", None)
+    if not token:
+        return {"error": "no token provided"}
+    
+    try:
+        decoded = await asyncio.to_thread(async_decode_jwt, token)
+        username = decoded.get("preferred_username", None)
+        if not username:
+            return {"error": "invalid token"}
+        oid = decoded.get("oid", None)
+        hashed_password = hashlib.sha256((username + SALT + oid).encode()).hexdigest()
+        if not validate_user(hashed_password):
+            # create user
+            conn = get_db_conn()
+            cur = conn.cursor()
+            hashed_password = hashed_password
+            cur.execute(
+                "INSERT INTO defog_users (username, hashed_password, token, user_type, is_premium) VALUES (%s, %s, %s, %s, %s)",
+                (username, hashed_password, DEFOG_API_KEY, "user", True),
+            )
+            conn.commit()
+            conn.close()
+        
+        return {"status": "success", "user_type": "user", "token": hashed_password}
+    except jwt.PyJWTError as e:
+        print(e)
+        return {"error": "invalid token"}
+
+    return {"status": "success"}
 
 @router.post("/login")
 async def login(request: Request):
