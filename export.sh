e4913a0dd8848087859a5f78c1f8a70827a8cc3b
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-01-26 20:13:03 +0800

    ensured that we are able to create clean out files, without any specific hostname

--- export.sh
+++ export.sh
@@ -1,4 +1,12 @@
+# load environment variables
+source .env.local
+
 # create an extensionless copy of all html files in the out/ directory
 for file in out/*.html; do
   cp "$file" "${file%.*}"
+done
+
+# replace all instances of NEXT_PUBLIC_AGENTS_ENDPOINT with DEFOGHOSTNAME
+find out -type f | while read file; do
+  LC_ALL=C sed -i '' "s/$NEXT_PUBLIC_AGENTS_ENDPOINT/DEFOGHOSTNAME/g" "$file"
 done
