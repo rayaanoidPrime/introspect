f73734e8d3abc97f999b91a0b0f10b522d39efcf
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-02-14 18:34:09 +0530

    final bugfix 🤦🏽‍♂️

--- agents-backend/src/agents/planner_executor/toolboxes/plots/tools.py
+++ agents-backend/src/agents/planner_executor/toolboxes/plots/tools.py
@@ -263,18 +263,21 @@ async def line_plot(
 
     if units is not None:
         df = (
-            df.groupby([i for i in relevant_columns if i != y_column])["y_column"]
+            df.groupby([i for i in relevant_columns if i != y_column])[y_column]
             .mean()
             .reset_index()
         )
 
     # sort the dataframe by the x_column
     if df[x_column].dtype == "object":
-        df["sort_key"] = df[x_column].apply(natural_sort)
-        df = df.sort_values(by="sort_key", key=natural_sort)
-        del df["sort_key"]
+        order = natural_sort(df[x_column].unique().tolist())
+        df[x_column] = pd.Categorical(df[x_column], categories=order, ordered=True)
+        if units is None:
+            df = df.sort_values(by=x_column)
+        else:
+            df = df.sort_values(by=[units, x_column])
     else:
-        df = df.sort_values(by=[x_column])
+        df = df.sort_values(by=x_column)
 
     chart_path = f"linecharts/linechart-{uuid4()}.png"
     fig, ax = plt.subplots()
