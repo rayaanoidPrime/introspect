4b00af964ea06730102114cd9c76c739460a22ff
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-02-20 14:28:13 +0800

    updated glossary

--- agents-backend/src/utils.py
+++ agents-backend/src/utils.py
@@ -122,9 +122,14 @@ def get_metadata():
     table_metadata_csv = redis_client.get("integration:metadata")
     client_description = "In this assignment, assume that you are a medical data analyst who is working with lab sample data for T cells of cancer patients."
     glossary = """- If you encounter the term `variable_value` in the metadata, it refers specifically to the column `variable_value`, and not a generic value. NEVER ask a question like "which variable name are you referring to"
-- Match the terms used by users to the terms used in the database schema. For example, if a user asks for Regulatory T Cells, and the database uses the term Tregs, then modify your response accordingly.
+- Match the terms used by users to the terms used in the database schema. For example, if a user asks for Regulatory T Cells, and the database had the term Tregs, then modify your response accordingly.
 - Recall that the term `reportable` refers to quantitative variables
-- When asking clarifying questions, ONLY use the information in the `column_name` column"""
+- When asking clarifying questions, ONLY use the information in the `column_name` column
+- When a user asks how something changes, they are typically looking to get a line chart or boxplot of a variable_value over time, using the visit_timepoint column
+- If a user asks how the expression of a gene changes over time, they are asking for a line chart of the variable_value over time, using the visit_timepoint column for data where the variable_name includes the gene name
+"""
+    # - If asked for proportion, refer to the flow_cytometry table
+    # - If asked for concentration, refer to the cytokine table
     return {
         "table_metadata_csv": table_metadata_csv,
         "client_description": client_description,
