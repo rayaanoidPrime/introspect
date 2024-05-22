e2342d48882f8d62cf12ffd3cf5ffb38b8ec78bc
Manas Sharma <manasdotsharma@gmail.com>
2024-05-22 15:05:34 +0530

    remove the defog-analysis-agent-rc folder. turn into one component called DefogAnalysisAgentStandalone

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,7 +4,6 @@ import { Context } from "../components/common/Context";
 import "../styles/base-styles.scss";
 import "../styles/doc-styles.scss";
 import "../styles/blocknote-styles.scss";
-import "../components/defog-analysis-agent-rc/styles/agent-rc-main.scss";
 
 export default function App({ Component, pageProps }) {
   const [context, setContext] = useState({});
