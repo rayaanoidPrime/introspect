bfdaf26bcd832cd545bbfe7192c113614f2d13d8
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-05-22 16:38:24 +0800

    remove env line from startup.sh

--- backend/docker-setup-files/startup.sh
+++ backend/docker-setup-files/startup.sh
@@ -1,6 +1,3 @@
 python3 create_admin_user.py
 python3 add_tools_to_db.py
-
-echo -e "\nOPENAI_API_KEY: \"$1\"\n" >> /agents-python-server/.env.yaml
-
 python3 -u -m hypercorn main:app -b 0.0.0.0:1235
