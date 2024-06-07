816dc3088e10f25ed5de435e00a0f8e4dc329172
Manas Sharma <manasdotsharma@gmail.com>
2024-06-07 12:26:08 +0530

    oops

--- frontend/components/docs/toolEditor/ToolEditor.jsx
+++ frontend/components/docs/toolEditor/ToolEditor.jsx
@@ -85,10 +85,6 @@ export function ToolEditor({
   const handleFinalSubmit = useCallback(async () => {
     setLoading(true);
     try {
-      if (!testingResults) {
-        message.error("Please run the tool before submitting");
-        return;
-      }
       const payload = {
         tool_name: toolName,
         function_name: functionName,
