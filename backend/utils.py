130d518b62f2a5cd982d63931a11bd5f1279df2b
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-06-12 18:53:40 +0800

    added a logging message to make_request

--- backend/utils.py
+++ backend/utils.py
@@ -296,6 +296,7 @@ class SqlExecutionError(Exception):
 
 
 async def make_request(url, payload):
+    print(f"Making request to {url}")
     async with httpx.AsyncClient() as client:
         r = await client.post(
             url,
