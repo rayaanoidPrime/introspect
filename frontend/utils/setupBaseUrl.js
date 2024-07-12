cda4b7e2108d43745a71b5f240fd94afc7f7b985
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-07-13 00:42:04 +0800

    fix setupBaseUrl so it works with npm run export

--- frontend/utils/setupBaseUrl.js
+++ frontend/utils/setupBaseUrl.js
@@ -5,16 +5,16 @@ const setupBaseUrl = (protocol, path) => {
 
   if (path !== "") {
     if (protocol === "ws") {
-      return `${process.env.NEXT_PUBLIC_AGENTS_ENDPOINT.replace(
+      return `${(process.env.NEXT_PUBLIC_AGENTS_ENDPOINT || "").replace(
         "http",
         "ws"
       )}/${path}`;
     } else {
-      return `${process.env.NEXT_PUBLIC_AGENTS_ENDPOINT}/${path}`;
+      return `${process.env.NEXT_PUBLIC_AGENTS_ENDPOINT || ""}/${path}`;
     }
   }
 
-  return `${process.env.NEXT_PUBLIC_AGENTS_ENDPOINT}`;
+  return `${process.env.NEXT_PUBLIC_AGENTS_ENDPOINT || ""}`;
 };
 
 export default setupBaseUrl;
