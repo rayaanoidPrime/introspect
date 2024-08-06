8d59c2a1ca9a2d6f0b35e80011a4588979256639
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-08-06 12:16:30 +0800

    added loading indicator, sqlserver, and cleaned up antd select

--- frontend/components/extract-metadata/DBCredentialsForm.js
+++ frontend/components/extract-metadata/DBCredentialsForm.js
@@ -9,6 +9,7 @@ const dbCredOptions = {
   snowflake: ["account", "warehouse", "user", "password"],
   databricks: ["server_hostname", "access_token", "http_path", "schema"],
   bigquery: [],
+  sqlserver: ["server", "database", "user", "password"],
 };
 
 const placeholders = {
@@ -32,6 +33,7 @@ const DbCredentialsForm = ({
 }) => {
   const [form] = Form.useForm();
   const [dbType, setDbType] = useState(dbData.db_type || "postgres");
+  const [loading, setLoading] = useState(false);
 
   const dbOptions = [
     { value: "postgres", label: "PostgreSQL" },
@@ -39,6 +41,7 @@ const DbCredentialsForm = ({
     { value: "snowflake", label: "Snowflake" },
     { value: "databricks", label: "Databricks" },
     { value: "bigquery", label: "Google BigQuery" },
+    { value: "sqlserver", label: "Microsoft SQL Server" },
   ];
 
   const handleDbTypeChange = (value) => {
@@ -83,13 +86,26 @@ const DbCredentialsForm = ({
 
   const handleSubmit = async (values) => {
     console.log("Received values of form: ", values);
-    const { db_type, ...db_creds } = values;
+    const { ...db_creds } = values;
     const payload = {
       db_creds: db_creds,
-      db_type: db_type || dbType,
+      db_type: dbType,
       token: token,
       key_name: apiKeyName,
     };
+
+    setLoading(true);
+    // first, check if the database connection is valid
+    const res = await validateDatabaseConnection(dbType, db_creds);
+    if (res.status !== "success") {
+      setDbConnectionStatus(false);
+      message.error(
+        "Could not connect to this database. Are you sure that database credentials are valid, and that your machine has DB access?"
+      );
+      setLoading(false);
+      return;
+    }
+
     try {
       const res = await fetch(
         setupBaseUrl("http", `integration/update_db_creds`),
@@ -109,9 +125,10 @@ const DbCredentialsForm = ({
     } catch (e) {
       console.log(e);
       message.error(
-        "Error fetching tables. Please check your docker logs for more information."
+        "Error fetching tables. Please check your logs for more information."
       );
     }
+    setLoading(false);
   };
 
   return (
@@ -126,15 +143,25 @@ const DbCredentialsForm = ({
         initialValues={{ db_type: dbData.db_type, ...dbData }}
         layout="vertical"
       >
-        <Form.Item name="db_type" label="Database Type">
-          <Select options={dbOptions} onChange={handleDbTypeChange} />
+        <Form.Item label="Database Type">
+          <Select
+            options={dbOptions}
+            onChange={handleDbTypeChange}
+            value={dbType}
+            loading={loading}
+            disabled={loading}
+          />
         </Form.Item>
         {dbCredOptions[dbType]?.map((field) => (
           <Form.Item
             key={field}
             name={field}
             label={field.charAt(0).toUpperCase() + field.slice(1)}
-            rules={[{ required: true, message: `Please input the ${field}!` }]}
+            rules={
+              dbType !== "sqlserver"
+                ? [{ required: true, message: `Please input the ${field}!` }]
+                : []
+            }
           >
             <Input
               type={
@@ -143,9 +170,12 @@ const DbCredentialsForm = ({
                   : "text"
               }
               placeholder={
-                placeholders?.[field] ? placeholders[field] : `Enter ${field}`
+                dbType === "sqlserver" && field === "database"
+                  ? "Leave blank if you want to query multiple databases inside the same server"
+                  : placeholders?.[field] || `Enter ${field}`
               }
               className="w-full p-2 border rounded bg-gray-50 text-gray-700 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              disabled={loading}
             />
           </Form.Item>
         ))}
@@ -154,6 +184,8 @@ const DbCredentialsForm = ({
             type="primary"
             htmlType="submit"
             className="w-full bg-blue-500 hover:bg-blue-600 text-white"
+            loading={loading}
+            disabled={loading}
           >
             Update
           </Button>
