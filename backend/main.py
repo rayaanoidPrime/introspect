bdd33dde72acfb493b1780ec42f4df34a747901c
jp <wongjingping@gmail.com>
2024-08-08 14:36:29 +0800

    add oracle router to main

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ from db_utils import (
     validate_user,
 )
 from generic_utils import get_api_key_from_key_name
-import integration_routes, query_routes, admin_routes, auth_routes, readiness_routes, csv_routes, feedback_routes, slack_routes
+import integration_routes, query_routes, admin_routes, auth_routes, readiness_routes, csv_routes, feedback_routes, slack_routes, oracle_routes
 
 manager = ConnectionManager()
 
@@ -35,6 +35,7 @@ app.include_router(doc_endpoints.router)
 app.include_router(csv_routes.router)
 app.include_router(feedback_routes.router)
 app.include_router(slack_routes.router)
+app.include_router(oracle_routes.router)
 
 origins = ["*"]
 app.add_middleware(
