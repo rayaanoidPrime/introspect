09a8691afebd22343a67125efb89f82d6859acaa
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-08-22 18:32:13 +0800

    refactor startup.sh to support multiple database types

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,4 +1,11 @@
-python3 create_sqlite_tables.py
+# if INTERAL_DB is sqlite, then create the sqlite tables. Else, create postgres tables
+if [ "$INTERNAL_DB" = "sqlite" ]; then
+  echo "Using sqlite as internal database"
+  python3 create_sql_tables.py --db_type sqlite
+else
+  echo "Using postgres as internal database"
+  python3 create_sql_tables.py --db_type sqlite
+fi
 python3 create_admin_user.py
 python3 add_tools_to_db.py
 # test if REDIS_INTERNAL_PORT is up, and sleep until it is
