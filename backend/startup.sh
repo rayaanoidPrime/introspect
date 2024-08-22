e69ac3573ee2ac4a12430845a984cc6acafb3565
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-08-22 18:51:04 +0800

    simplify startup.sh and wait until we can connect to the DB before executing the rest of the script

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,11 +1,10 @@
-# if INTERAL_DB is sqlite, then create the sqlite tables. Else, create postgres tables
-if [ "$INTERNAL_DB" = "sqlite" ]; then
-  echo "Using sqlite as internal database"
-  python3 create_sql_tables.py --db_type sqlite
-else
-  echo "Using postgres as internal database"
-  python3 create_sql_tables.py --db_type sqlite
-fi
+# test if postgres is up, and sleep until it is
+while ! nc -z $DBNAME $DBPORT; do
+  echo "Waiting for ${DB_NAME} to be available..."
+  sleep 1
+done
+
+python3 create_sql_tables.py
 python3 create_admin_user.py
 python3 add_tools_to_db.py
 # test if REDIS_INTERNAL_PORT is up, and sleep until it is
