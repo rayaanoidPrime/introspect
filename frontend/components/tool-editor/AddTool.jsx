23dda860bbb3208d3d43a0780e3908347ed37723
Manas Sharma <manasdotsharma@gmail.com>
2024-09-17 10:12:20 +0530

    change layout on lg instead of md

--- frontend/components/tool-editor/AddTool.jsx
+++ frontend/components/tool-editor/AddTool.jsx
@@ -219,8 +219,8 @@ export function AddTool({
         title: "Test",
         content: tool.code ? (
           <>
-            <div className="text-sm gap-8 text-gray-500 md:flex md:flex-row-reverse">
-              <ul className="my-4 w-full md:w-1/2 *:my-1">
+            <div className="text-sm gap-8 text-gray-500 lg:flex lg:flex-row-reverse">
+              <ul className="my-4 w-full lg:w-1/2 *:my-1">
                 <li className="list-disc">
                   We've created your tool based on the provided name and
                   description. You can test the tool with a question by typing
@@ -241,7 +241,7 @@ export function AddTool({
                   library.
                 </li>
               </ul>
-              <div className="grow w-1/2 my-4">
+              <div className="grow lg:w-1/2 my-4">
                 <ErrorBoundary>
                   <ToolFlow
                     toolName={tool.tool_name}
@@ -251,8 +251,8 @@ export function AddTool({
                 </ErrorBoundary>
               </div>
             </div>
-            <div className="divide-x mb-8 md:h-96 flex flex-row flex-wrap md:flex-nowrap gap-1">
-              <div className="w-full overflow-scroll relative px-2 bg-gray-800 md:w-5/12">
+            <div className="divide-x mb-8 lg:h-96 flex flex-row flex-wrap lg:flex-nowrap gap-1">
+              <div className="w-full overflow-scroll relative px-2 bg-gray-800 lg:w-5/12">
                 <div
                   className={
                     "w-full pt-2 pb-3 px-6 text-sm text-gray-500 sticky top-0 z-50 bg-gray-600 border-b border-gray-600 rounded-b-3xl shadow-md mb-4"
@@ -280,7 +280,7 @@ export function AddTool({
               </div>
               <div
                 className={twMerge(
-                  "relative w-full mt-8 px-2 pb-2 md:mt-0 md:w-7/12 bg-gray-200 overflow-scroll"
+                  "relative w-full mt-8 px-2 pb-2 lg:mt-0 lg:w-7/12 bg-gray-200 overflow-scroll"
                 )}
               >
                 <div
