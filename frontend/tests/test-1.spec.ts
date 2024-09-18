15cebff74829333e9fe99840e851fe67977b0b5c
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-09-18 11:38:49 +0800

    add overall test

--- frontend/tests/test-1.spec.ts
+++ frontend/tests/test-1.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  // TEST LOGIN
   await page.goto('http://localhost/');
   await page.goto('http://localhost/log-in');
   await page.getByLabel('Username').click();
@@ -8,6 +9,8 @@ test('test', async ({ page }) => {
   await page.getByLabel('Username').press('Tab');
   await page.getByLabel('Password').fill('admin');
   await page.getByRole('button', { name: 'Sign in' }).click();
+
+  // TEST ADD DB CREDENTIALS
   await page.getByText('PostgreSQL').click();
   await page.getByText('PostgreSQL').nth(1).click();
   await page.getByPlaceholder('host.docker.internal').click();
@@ -26,27 +29,54 @@ test('test', async ({ page }) => {
   await page.getByPlaceholder('Database Name').press('ControlOrMeta+a');
   await page.getByPlaceholder('Database Name').fill('restaurants');
   await page.getByRole('button', { name: 'Update' }).click();
+
+  // TEST UPDATE METADATA
   await page.getByLabel('Tabs').locator('div').nth(2).click();
   await page.getByRole('button', { name: 'Extract Table Metadata' }).click();
   await page.getByRole('button', { name: 'Save Changes' }).click();
   await page.getByRole('link', { name: 'Query Data' }).click();
+  // wait for 1 second
+
+  // TEST INITIAL QUERYING
+  await page.waitForTimeout(1000);
   await page.getByText('Restaurants').click();
   await page.getByPlaceholder('Type your question here').click();
   await page.getByPlaceholder('Type your question here').fill('what is the average rating by city?');
   await page.getByRole('button', { name: 'Ask' }).click();
-  await page.getByRole('button', { name: 'Click here or press enter to' }).click();
+  // see if a clarifying question is asked after 3 seconds
+  await page.waitForTimeout(3000);
+  const buttonFirstTry = page.getByRole('button', { name: 'Click here or press enter to' });
+  if (await buttonFirstTry.count() > 0) {
+    await buttonFirstTry.click();
+  }
+
+  // TEST POSITIVE FEEDBACK
   await page.getByText('👍').nth(1).click();
+
+  // TEST ADDING POSITIVE FEEDBACK TO GOLDEN QUERIES
   await page.getByRole('link', { name: 'View Feedback' }).click();
   await page.getByRole('button', { name: 'Add to Golden Queries' }).click();
+  
+  // TEST UPDATING GLOSSARY
   await page.getByRole('link', { name: 'Align Model' }).click();
   await page.getByRole('textbox').first().click();
   await page.getByRole('textbox').first().fill('- this is a compulsory instruction');
   await page.getByRole('textbox').nth(1).click();
   await page.getByRole('textbox').nth(1).fill('- this is a supplementary instruction');
   await page.getByRole('button', { name: 'Update Glossary' }).click();
+  
+  // TEST QUERYING AGAIN
   await page.getByRole('link', { name: 'Query Data' }).click();
+  // wait for 1 second
+  await page.waitForTimeout(1000);
   await page.getByText('Restaurants').click();
   await page.getByPlaceholder('Type your question here').click();
   await page.getByPlaceholder('Type your question here').fill('what is the average rating by city?');
-  await page.getByRole('button', { name: 'Click here or press enter to' }).click();
+  await page.getByRole('button', { name: 'Ask' }).click();
+  // see if a clarifying question is asked after 3 seconds
+  await page.waitForTimeout(3000);
+  const buttonSecondTry = page.getByRole('button', { name: 'Click here or press enter to' });
+  if (await buttonSecondTry.count() > 0) {
+    await buttonSecondTry.click();
+  }
 });
