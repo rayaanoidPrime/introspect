616350fd7b4f91292a364dcdd3caf130309b856a
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-09-20 22:21:54 +0800

    enable data analysis via bedrock

--- backend/agents/planner_executor/tool_helpers/core_functions.py
+++ backend/agents/planner_executor/tool_helpers/core_functions.py
@@ -3,6 +3,7 @@ import base64
 import os
 from generic_utils import make_request
 import os
+import json
 
 analysis_assets_dir = os.environ.get(
     "ANALYSIS_ASSETS_DIR", "/agent-assets/analysis-assets"
@@ -77,15 +78,53 @@ async def analyse_data(question: str, data_csv: str, sql: str, api_key: str) -> 
     if os.environ.get("ANALYZE_DATA", "no") != "yes":
         return ""
     else:
-        analysis = await make_request(
-            url=DEFOG_BASE_URL + "/oracle/gen_explorer_data_analysis",
-            data={
-                "api_key": api_key,
-                "user_question": question,
-                "generated_qn": question,
-                "sql": sql,
-                "data_csv": data_csv,
-                "sampled": False,
-            },
-        )
-        return analysis.get("summary", "")
+        if os.environ.get("ANALYZE_DATA_MODEL") == "defog":
+            analysis = await make_request(
+                url=DEFOG_BASE_URL + "/oracle/gen_explorer_data_analysis",
+                data={
+                    "api_key": api_key,
+                    "user_question": question,
+                    "generated_qn": question,
+                    "sql": sql,
+                    "data_csv": data_csv,
+                    "sampled": False,
+                },
+            )
+            return analysis.get("summary", "")
+        elif os.environ.get("ANALYZE_DATA_MODEL") == "bedrock":
+            import boto3
+
+            bedrock = boto3.client(service_name="bedrock-runtime")
+            model_id = "meta.llama3-70b-instruct-v1:0"
+            accept = "application/json"
+            contentType = "application/json"
+
+            body = json.dumps(
+                {
+                    "prompt": f"""<|begin_of_text|><|start_header_id|>user<|end_header_id|>
+
+Can you please give me the high-level trends (as bullet points that start with a hyphen) of data in a CSV? Note that this CSV was generated to answer the question: `{question}`
+
+This was the SQL query used to generate the table:
+{sql}
+
+This was the data generated:
+{data_csv}
+
+Do not use too much math in your analysis. Just tell me, at a high level, what the key insights are. Give me the trends as bullet points. No preamble or anything else.<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+Here is a summary of the high-level trends in the data:
+""",
+                    "max_gen_len": 600,
+                    "temperature": 0,
+                    "top_p": 1,
+                }
+            )
+
+            response = bedrock.invoke_model(
+                body=body, modelId=model_id, accept=accept, contentType=contentType
+            )
+            model_response = json.loads(response["body"].read())
+            print(model_response, flush=True)
+            generation = model_response["generation"]
+            return generation
