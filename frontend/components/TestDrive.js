6343f541c274535665201071f809780183ebc271
Rishabh Srivastava <rishabhsriv@gmail.com>
2024-09-27 19:09:00 +0800

    temporarily make searchBarDraggable `true`

--- frontend/components/TestDrive.js
+++ frontend/components/TestDrive.js
@@ -8,15 +8,11 @@ export function TestDrive({ token, dbs, devMode }) {
       token={token}
       // these are the ones that will be shown for new csvs uploaded
       uploadedCsvPredefinedQuestions={["Show me any 5 rows from the dataset"]}
-      searchBarDraggable={false}
-      searchBarClasses="sticky bottom-2"
+      searchBarDraggable={true}
+      // searchBarClasses="sticky bottom-2"
       dbs={dbs}
       disableMessages={false}
       devMode={devMode}
-      // apiEndpoint={"http://localhost:80"}
-      // token={
-      //   "bdbe4d376e6c8a53a791a86470b924c0715854bd353483523e3ab016eb55bcd0"
-      // }
     />
   );
 }
