482de02f332e839a8d9df6fe5f0a681db83857c0
Manas Sharma <manasdotsharma@gmail.com>
2024-11-15 12:57:14 +0530

    add tabs for tables

--- frontend/components/manage-sources/Source.tsx
+++ frontend/components/manage-sources/Source.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { DeleteOutlined } from "@ant-design/icons";
 import { Divider } from "antd";
-import { Table } from "@defogdotai/agents-ui-components/core-ui";
+import { Table, Tabs } from "@defogdotai/agents-ui-components/core-ui";
 
 interface SourceProps {
   link: string;
@@ -20,20 +20,33 @@ interface TableComponentProps {
   rows: string[][];
 }
 
-const TableComponent: React.FC<TableComponentProps> = ({ name, description, columns, rows }) => {
-  const displayName = name.replace("imported.", "");
+interface TabProps {
+  name: string;
+  content?: any;
+  classNames?: string;
+  headerClassNames?: string;
+}
+
+const TableComponent: React.FC<TableComponentProps> = ({
+  name,
+  description,
+  columns,
+  rows,
+}) => {
   // format columns and keys for Table component
-  const columnsWithIndex = columns.map((column) => ({ title: column, dataIndex: column }));
+  const columnsWithIndex = columns.map((column) => ({
+    title: column,
+    dataIndex: column,
+  }));
   const rowsWithColumnKeys = rows.map((row) => {
     return row.reduce((acc, value, index) => {
       acc[columns[index]] = value;
       return acc;
     }, {});
-  })
+  });
 
   return (
     <div>
-      <h3 className="text-l text-gray-600 mt-3">{displayName}</h3>
       <p className="text-s text-gray-500 italic mt-2 mb-3">{description}</p>
       <Table columns={columnsWithIndex} rows={rowsWithColumnKeys} />
     </div>
@@ -41,6 +54,17 @@ const TableComponent: React.FC<TableComponentProps> = ({ name, description, colu
 };
 
 const Source: React.FC<SourceProps> = ({ link, source, deleteSource }) => {
+  const tabs = useMemo<TabProps[]>(() => {
+    return source.tables.map((table, index) => {
+      const displayName = table?.name && table?.name?.replace("imported.", "");
+
+      return {
+        name: displayName,
+        content: <TableComponent key={index} {...table} />,
+      };
+    });
+  }, [source]);
+
   return (
     <div key={link}>
       <div className="flex">
@@ -52,15 +76,26 @@ const Source: React.FC<SourceProps> = ({ link, source, deleteSource }) => {
         >
           {source.source_title ? source.source_title : link}
         </a>
-        <DeleteOutlined className="ml-auto" onClick={() => deleteSource(link)} />
+        <DeleteOutlined
+          className="ml-auto"
+          onClick={() => deleteSource(link)}
+        />
       </div>
       <div>
         <p className="mt-2 text-gray-600">{source.source_summary}</p>
       </div>
-      {source.tables.length > 0 && <h2 className="text-xl font-semibold mt-4">Imported Tables</h2>}
-      {source.tables.map((table, index) => (
-        <TableComponent key={index} {...table} />
-      ))}
+
+      {source.tables.length > 0 && (
+        <h2 className="text-xl font-semibold mt-4 mb-4">Imported Tables</h2>
+      )}
+      {source.tables.length > 0 && (
+        <Tabs
+          size={"small"}
+          tabs={tabs}
+          defaultSelected={source.tables[0].name}
+        />
+      )}
+
       <Divider />
     </div>
   );
