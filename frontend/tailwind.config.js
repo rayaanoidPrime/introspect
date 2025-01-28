c605646dd177b4d05cf6162039485d0debe001a8
Manas Sharma <sharma.manas271196@gmail.com>
2025-01-28 10:45:23 +0530

    1.1.24

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./utils/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@defogdotai/agents-ui-components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@defogdotai/agents-ui-components/dist/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   // important: true,
   darkMode: "class",
