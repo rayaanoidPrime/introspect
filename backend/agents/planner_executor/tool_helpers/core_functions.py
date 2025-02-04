4253b89bc9a25952ce10201fed6c10d4828dbe1e
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-04 19:31:44 +0800

    remove unused func

--- backend/agents/planner_executor/tool_helpers/core_functions.py
+++ backend/agents/planner_executor/tool_helpers/core_functions.py
@@ -1,10 +1,7 @@
 import os
-from generic_utils import make_request, LOGGER
-import json
+from generic_utils import LOGGER
 from db_utils import redis_client
 from .analysis_prompts import (
-    DEFAULT_BEDROCK_MODEL,
-    DEFAULT_BEDROCK_PROMPT,
     DEFAULT_OPENAI_MODEL,
     DEFAULT_OPENAI_SYSTEM_PROMPT,
     DEFAULT_OPENAI_USER_PROMPT,
@@ -33,99 +30,6 @@ def safe_sql(query):
 
     return True
 
-
-async def analyse_data(question: str, data_csv: str, sql: str, api_key: str) -> str:
-    """
-    Generate a short summary of the results for the given qn.
-    """
-    if os.environ.get("ANALYZE_DATA", "no") != "yes":
-        return ""
-    else:
-        if os.environ.get("ANALYZE_DATA_MODEL") == "defog":
-            analysis = await make_request(
-                url=DEFOG_BASE_URL + "/agents/analyse_data",
-                data={
-                    "api_key": api_key,
-                    "question": question,
-                    "sql": sql,
-                    "data_csv": data_csv,
-                },
-            )
-            return analysis.get("model_analysis", "")
-        elif os.environ.get("ANALYZE_DATA_MODEL") == "bedrock":
-            import boto3
-
-            bedrock = boto3.client(service_name="bedrock-runtime")
-            model_id = redis_client.get("bedrock_model_id")
-            if model_id is None or model_id == "":
-                LOGGER.warning("BEDROCK_MODEL not set, skipping data analysis")
-                model_id = DEFAULT_BEDROCK_MODEL
-            accept = "application/json"
-            contentType = "application/json"
-            model_prompt = redis_client.get("bedrock_model_prompt")
-            if model_prompt is None or model_prompt == "":
-                model_prompt = DEFAULT_BEDROCK_PROMPT
-
-            body = json.dumps(
-                {
-                    "prompt": model_prompt.format(
-                        question=question, sql=sql, data_csv=data_csv
-                    ),
-                    "max_gen_len": 600,
-                    "temperature": 0,
-                    "top_p": 1,
-                }
-            )
-
-            response = await asyncio.to_thread(
-                bedrock.invoke_model,
-                body=body,
-                modelId=model_id,
-                accept=accept,
-                contentType=contentType,
-            )
-            model_response = json.loads(response["body"].read())
-            LOGGER.info(model_response)
-            generation = model_response["generation"]
-            return generation
-        elif os.environ.get("ANALYZE_DATA_MODEL") == "openai":
-            # if OPENAI_API_KEY is not set, return an error
-            if os.environ.get("OPENAI_API_KEY") is None:
-                return "You are using openai for analysing data, but OPENAI_API_KEY is not set. Please set it in the environment variables."
-            from openai import AsyncOpenAI
-
-            openai = AsyncOpenAI()
-            system_prompt = redis_client.get("openai_system_prompt")
-            if system_prompt is None or system_prompt == "":
-                system_prompt = DEFAULT_OPENAI_SYSTEM_PROMPT
-
-            user_prompt = redis_client.get("openai_user_prompt")
-            if user_prompt is None or user_prompt == "":
-                user_prompt = DEFAULT_OPENAI_USER_PROMPT
-
-            messages = [
-                {
-                    "role": "system",
-                    "content": system_prompt,
-                },
-                {
-                    "role": "user",
-                    "content": user_prompt.format(
-                        question=question, sql=sql, data_csv=data_csv
-                    ),
-                },
-            ]
-
-            response = await openai.chat.completions.create(
-                model=DEFAULT_OPENAI_MODEL,
-                messages=messages,
-                temperature=0,
-                max_tokens=600,
-                seed=42,
-            )
-
-            generation = response.choices[0].message.content.strip()
-            return generation
 
 async def analyse_data_streaming(question: str, data_csv: str, sql: str, api_key: str):
     """
