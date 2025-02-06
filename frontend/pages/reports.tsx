6a208a938aa28623845f4c803bf988109d8ab7a0
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-07 02:56:45 +0800

    clean up view-report page

--- frontend/pages/reports.tsx
+++ frontend/pages/reports.tsx
@@ -490,7 +490,7 @@ function OracleDashboard() {
                       className="text-purple-700 hover:text-purple-900 dark:text-purple-400 dark:hover:text-purple-300"
                       onClick={() =>
                         window.open(
-                          `/view-oracle-report?reportId=${report.report_id}&keyName=${apiKeyName}`,
+                          `/view-report?reportId=${report.report_id}&keyName=${apiKeyName}`,
                           "_blank"
                         )
                       }
