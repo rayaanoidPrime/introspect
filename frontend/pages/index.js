7c4b6d9bbdecfd7a201d37b931dff823fe4fb0f3
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-08 02:04:27 +0800

    add dark mode to index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -36,13 +36,13 @@ const QueryDatabase = () => {
     <>
       <Meta />
       <Scaffolding userType={userType}>
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl shadow-md">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+          <div className="max-w-md w-full space-y-8 p-10 bg-white dark:bg-gray-800 rounded-xl shadow-md">
             <div className="text-center">
-              <h1 className="text-3xl font-semibold text-gray-900 mb-4">
+              <h1 className="text-3xl font-semibold text-gray-900 dark:text-white mb-4">
                 Welcome to Defog!
               </h1>
-              <h3 className="text-lg text-gray-700">
+              <h3 className="text-lg text-gray-700 dark:text-gray-300">
                 Please wait while we log you in and redirect you to the right
                 page... <Spin />
               </h3>
