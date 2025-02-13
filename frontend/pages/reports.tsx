6406000d5a6001ee792e5e17e09cfc8f1cba80db
Manas Sharma <sharma.manas271196@gmail.com>
2025-02-13 11:54:18 +0530

    add scaffolding

--- frontend/pages/reports.tsx
+++ frontend/pages/reports.tsx
@@ -1,22 +1,43 @@
 "use client";
 import { useEffect, useState } from "react";
 import { OracleEmbed } from "@defogdotai/agents-ui-components/oracle";
+import Scaffolding from "$components/layout/Scaffolding";
+import Meta from "$components/layout/Meta";
 
 // Main Component
 export default function OracleDashboard() {
-  const [token, setToken] = useState<string>(null);
+  const [token, setToken] = useState<string>("");
+  const [userType, setUserType] = useState<string>("");
+  const [user, setUser] = useState<string>("");
 
   useEffect(() => {
     const token = localStorage.getItem("defogToken");
+    const userType = localStorage.getItem("defogUserType");
+    const user = localStorage.getItem("defogUser");
+    setUser(user);
+    setUserType(userType);
+    setToken(token);
     if (token) setToken(token);
   }, []);
 
   return (
-    token && (
-      <OracleEmbed
-        apiEndpoint={process.env.NEXT_PUBLIC_AGENTS_ENDPOINT || ""}
-        token={token}
-      />
+    token &&
+    userType &&
+    user && (
+      <div className="h-screen">
+        <Meta />
+        <Scaffolding
+          id={"query-data"}
+          userType={userType}
+          containerClassNames="max-h-screen h-screen flex flex-col"
+          contentClassNames="h-full max-h-full"
+        >
+          <OracleEmbed
+            apiEndpoint={process.env.NEXT_PUBLIC_AGENTS_ENDPOINT || ""}
+            token={token}
+          />
+        </Scaffolding>
+      </div>
     )
   );
 }
