2caf8bb6a4d01768cc5fe6e44423d49170a66b1e
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-18 12:06:54 +0800

    remove view feedback components

--- frontend/components/view-feedback/DisplayQuery.js
+++
@@ -1,9 +0,0 @@
-function DisplayQuery({ query }) {
-  return (
-    <div className="bg-gray-100 p-4 rounded-md font-mono max-h-48 overflow-auto">
-      <pre>{query}</pre>
-    </div>
-  );
-}
-
-export default DisplayQuery;
