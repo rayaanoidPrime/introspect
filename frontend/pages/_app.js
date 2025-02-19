9d64f45d81652499260a28a8af5a60c670d22e18
Muhammad18557 <muhammad.abdullah@u.yale-nus.edu.sg>
2025-02-19 18:02:42 +0800

    change antd theme with tailwind

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,29 +10,22 @@ import {
 import { UserContext } from "$components/context/UserContext";
 import { ThemeProvider } from "$components/context/ThemeContext";
 import ThemeToggle from "$components/ThemeToggle";
-import { ConfigProvider } from "antd";
 import { useTheme } from "$components/context/ThemeContext";
-import { lightTheme, darkTheme } from "../styles/theme";
 
 function AppContent({ Component, pageProps }) {
   const [context, setContext] = useState({});
   const [darkMode] = useTheme();
 
   return (
-    <ConfigProvider
-      theme={darkMode ? darkTheme : lightTheme}
-      wave={{ disabled: true }}
-    >
-      <UserContext.Provider value={[context, setContext]}>
-        <MessageManagerContext.Provider value={MessageManager()}>
-          <div className="min-h-screen bg-white dark:bg-dark-bg-primary text-primary-text dark:text-dark-text-primary transition-colors">
-            <MessageMonitor />
-            <Component {...pageProps} />
-            <ThemeToggle />
-          </div>
-        </MessageManagerContext.Provider>
-      </UserContext.Provider>
-    </ConfigProvider>
+    <UserContext.Provider value={[context, setContext]}>
+      <MessageManagerContext.Provider value={MessageManager()}>
+        <div className={`min-h-screen ${darkMode ? "dark" : ""} bg-white dark:bg-dark-bg-primary text-primary-text dark:text-dark-text-primary transition-colors`}>
+          <MessageMonitor />
+          <Component {...pageProps} />
+          <ThemeToggle />
+        </div>
+      </MessageManagerContext.Provider>
+    </UserContext.Provider>
   );
 }
 
