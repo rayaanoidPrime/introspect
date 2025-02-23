0c39a818ac5bf6d8170f9e6f3921dc6a6b282526
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-24 03:54:52 +0800

    add generate report

--- backend/oracle_routes.py
+++ backend/oracle_routes.py
@@ -1,27 +1,30 @@
 import os
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 from enum import Enum
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from utils_oracle import clarify_question, get_oracle_guidelines, set_oracle_guidelines
+import pandas as pd
+import re
+import time
+from utils_oracle import clarify_question, get_oracle_guidelines, set_oracle_guidelines, set_analysis, set_oracle_report, replace_sql_blocks
 from db_models import OracleGuidelines
 from db_config import engine
-from auth_utils import validate_user, validate_user_request
-from db_oracle_utils import (
-    get_analysis_status,
-)
+from auth_utils import validate_user_request
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
-from generic_utils import get_api_key_from_key_name
-from oracle.constants import TaskStage, TaskType
 from pydantic import BaseModel
 
 from utils_logging import LOGGER, save_and_log, save_timing
+from tools.analysis_tools import answer_question_from_database
+from defog.llm.utils import chat_async
+from llm_api import O3_MINI
 
-
-router = APIRouter()
+router = APIRouter(
+    dependencies=[Depends(validate_user_request)],
+    tags=["Oracle"],
+)
 
 DEFOG_BASE_URL = os.environ.get("DEFOG_BASE_URL", "https://api.defog.ai")
 
@@ -40,7 +43,7 @@ class SetGuidelinesRequest(BaseModel):
     db_name: Optional[str] = None
 
 
-@router.post("/oracle/set_guidelines", dependencies=[Depends(validate_user_request)])
+@router.post("/oracle/set_guidelines")
 async def set_guidelines(req: SetGuidelinesRequest):
     db_name = req.db_name
     if not db_name:
@@ -64,7 +67,7 @@ class GetGuidelinesRequest(BaseModel):
     db_name: Optional[str] = None
 
 
-@router.post("/oracle/get_guidelines", dependencies=[Depends(validate_user_request)])
+@router.post("/oracle/get_guidelines")
 async def get_guidelines(req: GetGuidelinesRequest):
     db_name = req.db_name
 
@@ -83,7 +86,6 @@ class ClarifyQuestionRequest(BaseModel):
     db_name: str
     token: str
     user_question: str
-    task_type: Optional[TaskType] = None
     answered_clarifications: List[Dict[str, Any]] = []
     clarification_guidelines: Optional[str] = None
 
@@ -94,7 +96,6 @@ class ClarifyQuestionRequest(BaseModel):
                     "key_name": "my_api_key",
                     "token": "user_token",
                     "user_question": "What are the sales trends?",
-                    "task_type": None,
                     "answered_clarifications": [],
                     "clarification_guidelines": "If unspecified, trends should be cover the last 3 years on a monthly basis.",
                 }
@@ -103,21 +104,18 @@ class ClarifyQuestionRequest(BaseModel):
     }
 
 
-@router.post("/oracle/clarify_question", dependencies=[Depends(validate_user_request)])
+@router.post("/oracle/clarify_question")
 async def clarify_question_endpoint(req: ClarifyQuestionRequest):
     """
     Given the question provided by the user, an optionally a list of answered
     clarifications, this endpoint will return a list of clarifications that still
-    need to be addressed. If this is a new question, the task_type will be inferred.
-    Otherwise, the task_type will be read from the request body and used to generate
-    the remaining clarifications.
+    need to be addressed.
 
     The response contains the following fields:
         clarifications: list[dict[str, str]] Each clarification dictionary will contain:
             - clarification: str
             - input_type: str (one of single_choice, multiple_choice, number, text)
             - options: list[str]
-        task_type: str
     """
     ts, timings = time.time(), []
     db_name = req.db_name
@@ -164,32 +162,97 @@ async def clarify_question_endpoint(req: ClarifyQuestionRequest):
                 "message": "Unable to generate clarifications",
             },
         )
-    clarify_response["task_type"] = "exploration"
     save_and_log(ts, "get_clarifications", timings)
     return JSONResponse(content=clarify_response)
 
-@router.post("/oracle/get_analysis_status")
-async def get_analysis_status_endpoint(req: AnalysisRequest):
-    user = await validate_user(req.token)
+class Clarification(BaseModel):
+    clarification: str
+    answer: Optional[Union[str, List[str]]] = None
+class GenerateReportRequest(BaseModel):
+    db_name: str
+    token: str
+    user_question: str
+    answered_clarifications: List[Clarification] = []
 
-    if not user:
-        return JSONResponse(
-            status_code=401,
-            content={
-                "error": "unauthorized",
-                "message": "Invalid username or password",
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "db_name": "db_name",
+                    "token": "user_token",
+                    "user_question": "User question",
+                    "answered_clarifications": [],
+                }
+            ]
+        }
+    }
+
+@router.post("/oracle/generate_report")
+async def generate_report(req: GenerateReportRequest):
+    db_name = req.db_name
+    user_question = req.user_question
+    answered_clarifications = req.answered_clarifications
+
+    tools = [answer_question_from_database]
+    analysis_response = await chat_async(
+        model=O3_MINI,
+        tools=tools,
+        messages=[
+            {
+                "role": "developer",
+                "content": "Formatting re-enabled\n\nWhen using tools, please use a single tool call for each question, and keep your questions short."
             },
-        )
+            {
+                "role": "user",
+                "content": f"""{user_question}\n\n Look in the database {db_name} for your answers, and feel free to continue asking multiple questions from the database if you need to. I would rather that you ask a lot of questions than too few.
 
-    api_key = await get_api_key_from_key_name(req.key_name)
+Try to aggregate data in clear and understandable buckets.
 
-    err, status = await get_analysis_status(
-        api_key=api_key,
-        analysis_id=req.analysis_id,
-        report_id=req.report_id,
+Please give your final answer as a descriptive report.
+
+For each point that you make in the report, please include the relevant SQL query that was used to generate the data for it. You can include as many SQL queries as you want, including multiple SQL queries for one point.
+"""
+}],
     )
 
-    if err:
-        return JSONResponse(status_code=500, content={"error": err})
+    tool_outputs = analysis_response.tool_outputs
+    sql_to_analysis_id = {}
+    for tool_output in tool_outputs:
+        # save this analysis in the Postgres DB
+        tool_id = tool_output["tool_call_id"]
+        data_csv = pd.DataFrame(tool_output["result"].rows, columns=tool_output["result"].colnames).to_csv(index=False)
+        analysis_question = tool_output["args"]["question"]
+        mdx = f"We tried to answer the following question: `{analysis_question}`. Data to answer this question is below."
 
-    return JSONResponse(content={"status": status})
+        sql = tool_output["result"].sql
+        # convert the sql to lowercase, and remove all newline + multiple whitespace
+        stripped_sql = re.sub(r'\s+', ' ', sql.lower())
+        sql_to_analysis_id[stripped_sql] = tool_id
+
+        await set_analysis(
+            analysis_id=tool_id,
+            db_name=db_name,
+            sql=sql,
+            csv=data_csv,
+            mdx=mdx,
+        )
+    
+    main_content = analysis_response.content
+
+    # FIND ALL ```sql ... ``` inside the tool and replace with the corresponding analysis_id
+    main_content = replace_sql_blocks(main_content, sql_to_analysis_id)
+    analysis_ids = [i["tool_call_id"] for i in tool_outputs]
+
+    # save to oracle_reports table
+    await set_oracle_report(
+        db_name=db_name,
+        report_name=user_question,
+        inputs=req.model_dump(),
+        mdx=main_content,
+        analysis_ids=analysis_ids,
+    )
+
+    return JSONResponse(
+        mdx=main_content,
+        analysis_ids=analysis_ids,
+    )
