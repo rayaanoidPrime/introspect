42cdb380e9b652006805e5ef57fd6ddaff2906bd
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-02-24 03:54:12 +0800

    fix function by adding db type creds

--- backend/tools/analysis_tools.py
+++ backend/tools/analysis_tools.py
@@ -1,14 +1,20 @@
 from tools.analysis_models import AnswerQuestionFromDatabaseInput, AnswerQuestionFromDatabaseOutput
 from utils_sql import generate_sql_query
 from defog.query import async_execute_query_once
+from db_utils import get_db_type_creds
 
 async def answer_question_from_database(input: AnswerQuestionFromDatabaseInput) -> AnswerQuestionFromDatabaseOutput:
     """
-    Given a *single* question for a *single* database, this function will first generate a SQL query to answer the question.
+    Given a *single* question for a *single* database, this function will generate a SQL query to answer the question.
     Then, it will execute the SQL query on the database and return the results.
+
+    IMPORTANT: this function will only take in a single question. Do not try to handle multiple questions in the same call.
     """
     question = input.question
     db_name = input.db_name
+
+    res = await get_db_type_creds(db_name)
+    db_type, db_creds = res
     
     print(question, flush=True)
 
